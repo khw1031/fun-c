d9b8d084062bc21e27d82e2fa266fb89a06f208b
khw1031 <khw1031@gmail.com>
2024-06-25 11:06:44 +0900

    update

--- src/toNumber/index.ts
+++ src/toNumber/index.ts
@@ -1,20 +1,30 @@
 import { filter } from "../filter";
 import { head } from "../head";
+import { isNumber } from "../isNumber";
 import { map } from "../map";
 import { pipe } from "../pipe";
+import { tap } from "../tap";
+import { toArray } from "../toArray";
 import { toString } from "../toString";
 
-const isCurrencyForamt = (str: string) =>
+const isCurrencyFormat = (str: string) =>
   /^\d{1,3}(,\d{3})*(\.\d+)?$/.test(str);
+const isMinusCurrentyFormat = (str: string) =>
+  /^-\d{1,3}(,\d{3})*(\.\d+)?$/.test(str);
 
 const currencyToNumber = (str: string) => +str.replace(/,/g, "");
 
 export const toNumber = <T>(value: T): number => {
+  if (isNumber(value)) {
+    return value;
+  }
   return (
     pipe(
       [value],
       map(toString),
-      filter(isCurrencyForamt),
+      filter(
+        (numStr) => isCurrencyFormat(numStr) || isMinusCurrentyFormat(numStr),
+      ),
       map(currencyToNumber),
       map((n) => +n),
       head,
--- src/toNumber/toNumber.spec.ts
+++ src/toNumber/toNumber.spec.ts
@@ -1,8 +1,11 @@
 import { toNumber } from ".";
 
 describe("toNumber", () => {
-  it("number", () => {
+  it("number 1", () => {
     expect(toNumber(1)).toBe(1);
+  });
+  it("number 1000000", () => {
+    expect(toNumber(1000000)).toBe(1000000);
   });
   it("string 1", () => {
     expect(toNumber("1")).toBe(1);
@@ -21,5 +24,8 @@ describe("toNumber", () => {
   });
   it("string 1000,000", () => {
     expect(toNumber("1000,000")).toBe(NaN);
+  });
+  it("string -1,000,000", () => {
+    expect(toNumber("-1,000,000")).toBe(-1000000);
   });
 });
